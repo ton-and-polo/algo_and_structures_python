235823d0107e66c0f44d8364ecd1e4ea3eec7a89
ton-and-polo <ton.and.polo@gmail.com>
2019-03-08 21:36:59 +0300

    Done 8th task

--- Lesson_1/8.py
+++ Lesson_1/8.py
@@ -3,3 +3,9 @@
 #Год является високосным в двух случаях: либо он кратен 4,
 # но при этом не кратен 100, либо кратен 400.
 
+year = int(input('Enter a year: '))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print('Leap year')
+else:
+    print('Non leap year')
