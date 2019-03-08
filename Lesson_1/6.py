2ef9a5c77f0cb802a3f91160b40fbdae4f913de2
ton-and-polo <ton.and.polo@gmail.com>
2019-03-08 21:25:54 +0300

    Done 6th task

--- Lesson_1/6.py
+++ Lesson_1/6.py
@@ -1,1 +1,6 @@
 # 6.	Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+
+from string import ascii_lowercase
+
+user_input = int(input('Enter number (1-24): '))
+print(ascii_lowercase[user_input - 1])
