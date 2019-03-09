0a110962ffb23553a2ff863a89735f8f9e135df2
ton-and-polo <ton.and.polo@gmail.com>
2019-03-09 15:47:51 +0300

    Done 4th task

--- Lesson_1/4.py
+++ Lesson_1/4.py
@@ -8,3 +8,19 @@
 то вводятся эти символы. Программа должна вывести на экран любой
 символ алфавита от 'a' до 'f' включительно.
 """
+import random
+import string
+
+print('[1] - random integer\n[2] - random real number\n[3] - random char')
+user_input = int(input('Enter num:\n'))
+user_range = input('Enter range (x,y): ')
+x = int(user_range.split(',')[0])
+y = int(user_range.split(',')[1])
+
+if user_input == 1:
+    print(random.randint(x, y))
+elif user_input == 2:
+    print(random.uniform(x, y))
+elif user_input == 3:
+    # string.punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+    print(string.punctuation[random.randint(x, y)])
