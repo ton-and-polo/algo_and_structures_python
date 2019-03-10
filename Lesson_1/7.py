96fadb7c01436bd065caa8f54aa4cbf4a2eab4e9
ton-and-polo <ton.and.polo@gmail.com>
2019-03-10 17:38:41 +0300

    Done 7th task

--- Lesson_1/7.py
+++ Lesson_1/7.py
@@ -4,3 +4,18 @@
 треугольник существует, то определить, является ли он
 разносторонним, равнобедренным или равносторонним.
 """
+
+a = int(input('Enter length a: '))
+b = int(input('Enter length b: '))
+c = int(input('Enter length c: '))
+
+if (a + b > c) or (b + c > a) or (c + a > b):
+    if a != b and b != c:
+        print('abc - miscellaneous triangle')
+    elif a == b and b == c:
+        print('abc - equilateral triangle')
+    elif a == b or b == c or c == a:
+        print('abc - isosceles triangle')
+
+else:
+    print('abc - not a triangle')
