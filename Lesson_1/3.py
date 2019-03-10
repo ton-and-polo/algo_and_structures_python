0ea8e0b67716ec8ef1cb39a67823feda1de413df
ton-and-polo <ton.and.polo@gmail.com>
2019-03-11 00:48:19 +0300

    Change type for x,y var and style output

--- Lesson_1/3.py
+++ Lesson_1/3.py
@@ -4,16 +4,17 @@
 point_1 = (input('Enter (x1,y1): ')).split(',')  # '-7,-5'
 point_2 = (input('Enter (x1,y1): ')).split(',')  # '2,1'
 
-x1 = float(point_1[0])
-y1 = float(point_1[1])
+x1 = int(point_1[0])
+y1 = int(point_1[1])
 
-x2 = float(point_2[0])
-y2 = float(point_2[1])
+x2 = int(point_2[0])
+y2 = int(point_2[1])
 
 k = (y1 - y2) / (x1 - x2)
 b = y2 - k * x2
 
 if b >= 0:
-    print('y={0:.1f}x+{1:.1f}'.format(k, b))
+    print(f'y={round(k, 1)}x+{round(b, 1)}')
 else:
-    print('y={0:.1f}x{1:.1f}'.format(k, b))
+    print(f'y={round(k, 1)}x{round(b, 1)}')
+
