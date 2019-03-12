d526f60119bf68f5f021b3dc25383adde7bb95b3
ton-and-polo <ton.and.polo@gmail.com>
2019-03-12 20:11:13 +0300

    Done 6th task

--- Lesson_2/6.py
+++ Lesson_2/6.py
@@ -5,3 +5,48 @@
 число, чем то, что загадано. Если за 10 попыток число не отгадано,
 то вывести загаданное число.
 """
+from random import randint
+
+# Using loop:
+
+# Game conditions:
+max_try = 10
+answer = randint(0, 100)
+
+for i in range(1, 11):
+    user_answer = int(input('Guess num (0-100): '))
+    if user_answer == answer:
+        print('You win!')
+        break
+    elif i == max_try:
+        print(f'You louse! Correct answer: {answer}')
+
+    else:
+        if user_answer > answer:
+            print(f'{user_answer} > answer')
+        elif user_answer < answer:
+            print(f'{user_answer} < answer')
+
+
+# Using recursion:
+
+
+def guess_game(answer=randint(0, 100), user_try=1):
+    user_answer = int(input('Guess num (0-100): '))
+
+    if user_answer == answer:
+        return 'You win!'
+    elif user_try == 10:
+        return f'You louse! Correct answer: {answer}'
+    else:
+        if user_answer > answer:
+            print(f'{user_answer} > answer')
+        elif user_answer < answer:
+            print(f'{user_answer} < answer')
+
+        user_try += 1
+
+        return guess_game(answer, user_try)
+
+
+print(guess_game())
