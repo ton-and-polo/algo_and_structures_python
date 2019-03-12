616d017c44698b1db85a3053a205b7b6280bd780
ton-and-polo <ton.and.polo@gmail.com>
2019-03-12 18:25:28 +0300

    Done 3rd task

--- Lesson_2/3.py
+++ Lesson_2/3.py
@@ -3,3 +3,27 @@
 цифр и вывести на экран. Например, если введено число 3486,
  то надо вывести число 6843.
 """
+
+user_num = input('Enter a num: ')  # '6843'
+
+# Using loop:
+
+result = ''
+max_index = len(user_num) - 1
+for index in range(len(user_num)):
+    result += user_num[max_index - index]
+print(result)
+
+
+# Using recursion:
+
+
+def revers(number, revers_number=''):
+    if number:
+        revers_number += number[-1]
+        return revers(number[:-1], revers_number)
+    else:
+        return revers_number
+
+
+print(revers(user_num))
