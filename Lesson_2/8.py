748bca176680d06c6034bb105ce4c23c3b3bc64c
ton-and-polo <ton.and.polo@gmail.com>
2019-03-13 11:48:40 +0300

    Done 8th task

--- Lesson_2/8.py
+++ Lesson_2/8.py
@@ -3,3 +3,29 @@
  последовательности чисел. Количество вводимых чисел и цифра,
  которую необходимо посчитать, задаются вводом с клавиатуры.
 """
+
+user_num = input('Enter number: ')
+user_digit = input('Enter digit: ')
+
+# Using loop:
+
+result = 0
+for num in user_num:
+    if num == user_digit:
+        result += 1
+print(result)
+
+
+# Using recursion:
+
+
+def digit_counter(number, digit, result=0):
+    if number:
+        if number[0] == digit:
+            result += 1
+        return digit_counter(number[1:], digit, result)
+    else:
+        return result
+
+
+print(digit_counter(user_num, user_digit))
