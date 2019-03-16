e0627a4f5c59e0f7afbad9d5349416198ced0e71
ton-and-polo <ton.and.polo@gmail.com>
2019-03-16 13:46:28 +0300

    Done 4th task

--- Lesson_3/4.py
+++ Lesson_3/4.py
@@ -1,1 +1,8 @@
 # 4.	Определить, какое число в массиве встречается чаще всего.
+from random import randint
+from collections import Counter
+
+
+my_list = [randint(0, 100) for _ in range(50)]
+result = Counter(my_list).most_common(1)
+print(f'num: {result[0][0]}\nfrequency: {result[0][1]}')
