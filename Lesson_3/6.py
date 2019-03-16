ced64a0e02d44209ee3cb69f20b9d6394626cfcc
ton-and-polo <ton.and.polo@gmail.com>
2019-03-16 20:45:24 +0300

    Done 6th task

--- Lesson_3/6.py
+++ Lesson_3/6.py
@@ -3,3 +3,8 @@
 между минимальным и максимальным элементами.
 Сами минимальный и максимальный элементы в сумму не включать.
 """
+
+my_list = [8, 3, 15, 6, 4, 2]
+
+result = [i for i in my_list if i != max(my_list) and i != min(my_list)]
+print(f'The sum of all items in the array without max and min is: {sum(result)}')
