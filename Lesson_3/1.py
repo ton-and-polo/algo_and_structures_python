5458c8cacad05907e487f2d69c4e93e85735675e
ton-and-polo <ton.and.polo@gmail.com>
2019-03-16 20:30:06 +0300

    Chage output for result in 1st task

--- Lesson_3/1.py
+++ Lesson_3/1.py
@@ -1,4 +1,5 @@
-# 1.	В диапазоне натуральных чисел от 2 до 99 определить,
+# 1. В диапазоне натуральных чисел от 2 до 99 определить,
 # сколько из них кратны каждому из чисел в диапазоне от 2 до 9.
 
-print(len([num for num in range(2, 100) for divider in range(2, 10) if num % divider == 0]))
+result = len([num for num in range(2, 100) for divider in range(2, 10) if num % divider == 0])
+print(f'There is {result} multiples in range from two to nine for an array of numbers [2, 3, 4, ... 99].')
