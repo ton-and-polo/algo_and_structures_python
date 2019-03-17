75fc5ec2d6a03c31dbc21c4d7330c5eed863a886
ton-and-polo <ton.and.polo@gmail.com>
2019-03-17 12:17:02 +0300

    Add count() solution to 4th task

--- Lesson_3/4.py
+++ Lesson_3/4.py
@@ -4,5 +4,10 @@ from collections import Counter
 
 
 my_list = [randint(0, 100) for _ in range(50)]
-result = Counter(my_list).most_common(1)
-print(f'num: {result[0][0]}\nfrequency: {result[0][1]}')
+
+result_1 = Counter(my_list).most_common(1)
+print(f'Frequency using Counter: {result_1[0][1]}')
+
+
+result_2 = [my_list.count(i) for i in my_list]
+print(f'Frequency using count(): {max(result_2)}')
