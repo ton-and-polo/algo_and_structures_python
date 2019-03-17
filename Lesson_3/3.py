f3f9f64ff4128f8e51fa849b8a432ad82124a8f7
ton-and-polo <ton.and.polo@gmail.com>
2019-03-17 11:39:56 +0300

    Done 3rd task

--- Lesson_3/3.py
+++ Lesson_3/3.py
@@ -1,2 +1,9 @@
-#3.	В массиве случайных целых чисел поменять местами минимальный
+# 3.	В массиве случайных целых чисел поменять местами минимальный
 # и максимальный элементы.
+
+my_list = [8, 3, 15, 6, 4, 2]
+
+a, b = my_list.index(max(my_list)), my_list.index(min(my_list))
+my_list[a], my_list[b] = my_list[b], my_list[a]
+
+print(f'Swapped min and max elements: {my_list}')
