cfe41d052957eb0ee320b98240ce7013ad669c37
ton-and-polo <ton.and.polo@gmail.com>
2019-03-18 11:57:29 +0300

    Done 8th task

--- Lesson_3/8.py
+++ Lesson_3/8.py
@@ -4,3 +4,13 @@
 записывать ее в последнюю ячейку строки.
 В конце следует вывести полученную матрицу.
 """
+from random import randint
+
+row = 4
+col = 4
+
+my_list = [[randint(0, 9) for i in range(row)] for i in range(col)]
+
+for i in range(len(my_list)):
+    my_list[i].append(sum([num for num in my_list[i]]))
+    print(my_list[i])
