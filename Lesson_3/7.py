031a71abf5516de9fa0034215973834187751fbb
ton-and-polo <ton.and.polo@gmail.com>
2019-03-18 11:17:40 +0300

    Done 7th task

--- Lesson_3/7.py
+++ Lesson_3/7.py
@@ -3,3 +3,16 @@
 Они могут быть как равны между собой (оба являться минимальными),
  так и различаться.
 """
+my_list = [8, 3, 15, 6, 4, 2, 5, 3]
+
+min_1 = min(my_list)
+min_1_frequency = len([i for i in my_list if i == min(my_list)])
+
+
+if min_1_frequency > 1:
+    print(f'min: {min_1}, frequency: {min_1_frequency}')
+else:
+    print(f'min_1: {min_1}, frequency: {min_1_frequency}')
+    min_2 = min([i for i in my_list if i != min_1])
+    min_2_frequency = len([i for i in my_list if i == min_2])
+    print(f'min_2: {min_2}, frequency: {min_2_frequency}')
