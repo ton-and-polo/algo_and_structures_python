0c4286e785b31f998c0a00f960e96085c22a5aaf
ton-and-polo <ton.and.polo@gmail.com>
2019-03-27 18:36:47 +0300

    Done 2nd task

--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,53 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+import re
+import collections
+
+
+# Using functional programming:
+num_1 = int('a2', 16)
+num_2 = int('c4f', 16)
+
+addition = re.findall(r'\w', str(hex(num_1 + num_2))[2:])
+multiplication = re.findall(r'\w', str(hex(num_1 * num_2))[2:])
+
+print(f'Functional programming\naddition: {addition}\nmultiplication: {multiplication}\n')
+
+
+# Using object-oriented programming:
+class MyHex:
+    def __init__(self, num):
+        self.num = int(num, 16)
+
+    def __add__(self, other):
+        return hex(self.num + other.num)
+
+    def __mul__(self, other):
+        return hex(self.num * other.num)
+
+
+x = MyHex('a2')
+y = MyHex('c4f')
+
+print(f'Object-oriented programming\naddition: {list(x + y)[2:]}\nmultiplication: {list(x * y)[2:]}\n')
+
+
+# Using collections (defaultdict):
+my_collection = collections.defaultdict(list)
+numbers = ['a2', 'c4f']
+for i in range(len(numbers)):
+    my_collection[i].append(int(numbers[i], 16))
+
+
+def multiplication(my_list):
+    result = 1
+    for i in my_list:
+        result *= i
+    return result
+
+
+addition = list(hex(sum([i[0] for i in my_collection.values()])))[2:]
+multiplication = list(hex(multiplication([i[0] for i in my_collection.values()])))[2:]
+
+print(f'Using collections\naddition: {addition}\nmultiplication: {multiplication}')
