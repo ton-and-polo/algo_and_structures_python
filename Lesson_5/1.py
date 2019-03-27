501f6d09f0cd30171b94a1ada6bb97329bc93c1d
ton-and-polo <ton.and.polo@gmail.com>
2019-03-27 17:31:55 +0300

    Done 1st task

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,24 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+import collections
+
+
+branches = int(input('Enter quantity of branches: '))
+branches_dict = dict()
+
+for i in range(1, branches + 1):
+    branch_name = input('Enter branch name: ')
+    branch_profit = [int(input(f'Enter {branch_name} profit_{i + 1}: ')) for i in range(4)]
+
+    branches_dict[branch_name] = branch_profit
+
+my_collection = collections.Counter(branches_dict)
+
+
+avg_profit = int(sum([sum(my_collection[branch]) for branch in my_collection]) / len(my_collection))
+
+above_avg = [branch for branch in my_collection if sum(my_collection[branch]) >= avg_profit]
+below_avg = [branch for branch in my_collection if sum(my_collection[branch]) < avg_profit]
+
+print(f'above profit: {above_avg}\nbelow profit: {below_avg}')
