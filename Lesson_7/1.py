e6bcbef4f5fcb5099de8a27ec03bb145f0290d4d
ton-and-polo <ton.and.polo@gmail.com>
2019-03-29 12:49:03 +0300

    Done 1st task

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,20 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+import random
+
+my_list = [random.randint(-100, 100) for i in range(10)]
+
+
+def bubble_sorting(list):
+    index = 1
+    while index < len(list):
+        for i in range(len(list)-1):
+            if list[i] < list[i + 1]:
+                list[i + 1], list[i] = list[i], list[i + 1]
+        index += 1
+
+    return list
+
+
+print(bubble_sorting(my_list))
