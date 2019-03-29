cf2cb807ade7abf3fe599378892b6547dd771b67
ton-and-polo <ton.and.polo@gmail.com>
2019-03-29 13:13:13 +0300

    Improve 1st task

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -9,15 +9,19 @@ import random
 my_list = [random.randint(-100, 100) for i in range(10)]
 
 
-def bubble_sorting(list):
-    index = 1
-    while index < len(list):
-        for i in range(len(list)-1):
-            if list[i] < list[i + 1]:
-                list[i + 1], list[i] = list[i], list[i + 1]
-        index += 1
+def bubble_sort(list):
+
+    length_list = len(list) - 1
+    sorted = False
+
+    while not sorted:
+        sorted = True
+        for i in range(length_list):
+            if list[i] < list[i+1]:
+                list[i+1], list[i] = list[i], list[i + 1]
+                sorted = False
 
     return list
 
 
-print(bubble_sorting(my_list))
+print(bubble_sort(my_list))
