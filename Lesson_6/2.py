7377a393fa6c54fa9a10eefe8ee41fd08144ca99
ton-and-polo <ton.and.polo@gmail.com>
2019-03-30 13:33:59 +0300

    Done 2nd task

--- Lesson_6/2.py
+++ Lesson_6/2.py
@@ -5,3 +5,20 @@
 атрибутов. Сравнить, сколько выделяется памяти для хранения атрибутов обоих
 классов.
 """
+from sys import getsizeof
+
+
+class NotSlotted:
+    def __init__(self, name):
+        self.name = name
+
+
+class Slotted(object):
+    __slots__ = 'name'
+
+    def __init__(self, name):
+        self.name = name
+
+
+if getsizeof(NotSlotted('Mike')) > getsizeof(Slotted('Mike')):
+    print('__slots__ saves memory!')
