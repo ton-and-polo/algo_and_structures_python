403071799b47b4811f524c83bc9616f984caa51e
ton-and-polo <ton.and.polo@gmail.com>
2019-04-01 21:07:56 +0300

    Done 3rd task

--- Lesson_7/3.py
+++ Lesson_7/3.py
@@ -6,3 +6,63 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
  который не рассматривался на уроках
 """
+from random import randint
+from statistics import median
+
+
+my_list = [randint(-10, 10) for i in range(randint(1, 100))]
+
+
+# Built-in solution (median()):
+print(median(my_list))
+
+
+# Cocktail shaker and my_median solution:
+def shaker_sort(my_list):
+    left = 0
+    right = len(my_list) - 1
+
+    swap = False
+    while not swap and right - left > 1:
+        swap = True
+
+        for i in range(right, left, -1):
+            if my_list[i - 1] > my_list[i]:
+                my_list[i], my_list[i - 1] = my_list[i - 1], my_list[i]
+                swap = False
+        left += 1
+
+        for i in range(left, right):
+            if my_list[i + 1] < my_list[i]:
+                my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+                swap = False
+        right -= 1
+
+    return my_list
+
+
+def my_median(my_list):
+    if len(my_list) == 1:
+        return my_list[0]
+
+    my_list = shaker_sort(my_list)
+
+    x = my_list[(len(my_list) - 1) // 2]
+    y = my_list[(len(my_list)) // 2]
+    diff = y - x
+
+    if len(my_list) % 2 == 0:
+        if diff == 0:
+            result = my_list[len(my_list)//2]
+        elif abs(diff) == 1 or abs(diff) >= 3:
+            result = x + (diff/2)
+        elif abs(diff) < 3:
+            result = diff
+
+    elif len(my_list) % 2 != 0:
+        result = my_list[len(my_list)//2]
+
+    return float(result)
+
+
+print(my_median(my_list))
